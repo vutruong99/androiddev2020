4b2fff50c9ff6541b64880bc608da82109865f13
Thi Vu <truongsithivu99@gmail.com>
2019-12-03 11:16:44 +0700

    PRACTICAL WORK 12B

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,17 +1,24 @@
 package vn.edu.usth.weather;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TableLayout;
 import android.support.design.widget.TabLayout;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,6 +30,16 @@ import java.io.OutputStream;
 public class WeatherActivity extends AppCompatActivity {
 
     MediaPlayer mp;
+
+    final Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+// This method is executed in main thread
+            String content = msg.getData().getString("server_response");
+            Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT).show();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,12 +61,55 @@ public class WeatherActivity extends AppCompatActivity {
         mp = MediaPlayer.create(getApplicationContext(), R.raw.humble);
 
         mp.start();
+
     }
 
 
+
+    //Labwork 12
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.refresh:
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+// this method is run in a worker thread
+                        try {
+// wait for 5 seconds to simulate a long network access
+                            Thread.sleep(5000);
+                        }
+                        catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+// Assume that we got our data from server
+                        Bundle bundle = new Bundle();
+                        bundle.putString("server_response", "some sample json here");
+// notify main thread
+                        Message msg = new Message();
+                        msg.setData(bundle);
+                        handler.sendMessage(msg);
+                    }
+                });
+                t.start();
+                return true;
+            case R.id.settings:
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //labwork 11
     private void writeExternal() {
         String filename = "humble.mp3";
-        String filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data";
+        String filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/";
 
         try {
             InputStream is = getApplicationContext().getResources().openRawResource(R.raw.humble);
